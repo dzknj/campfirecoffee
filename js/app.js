bf24de48e08182e6e192f09c5403f54325830002
David Zalk <dzknj@outlook.com>
2016-01-13 16:10:33 -0800

    made three tables of data

--- js/app.js
+++ js/app.js
@@ -10,6 +10,10 @@ var ShopLocation = function(location,minCust,maxCust,cupsCust,poundsCust) {
   this.randomNumberOfCust = function(){
     return Math.floor(Math.random()*((this.maxCustPerHour-this.minCustPerHour)+1)+this.minCustPerHour);
   };
+  this.cupsPerHour = function(){
+    var hourlyCustomer = this.randomNumberOfCust();
+    return (this.hourlyCustomer * this.cupsPerCust);
+  }
   this.totalPounds = function(){
       var hourlyCustomer = this.randomNumberOfCust();
       return (((hourlyCustomer*this.cupsPerCust)/20)+(hourlyCustomer*this.poundsPerCust));
@@ -74,6 +78,95 @@ var createTable =  function(){
     tableRow.appendChild(tableHead);
       document.body.appendChild(table);
 };
+var createCustPerHourTable =  function(){
+  var section = document.getElementById('test');
+  var hours = ['6:00','7:00','8:00','9:00','10:00','11:00','12:00','1:00','2:00','3:00','4:00','5:00','6:00','7:00','8:00'];
+  var table = document.createElement('table');
+  var tableRow = document.createElement('tr');
+  var tableHead = document.createElement('th');
+  tableHead.textContent = 'Location';
+  tableRow.appendChild(tableHead);
+
+  for (var i = 0; i < hours.length; i++){
+    var tableHeadHours = document.createElement('th');
+    tableHeadHours.textContent = hours[i];
+    tableRow.appendChild(tableHeadHours);
+
+  }
+    table.appendChild(tableRow);
+//    document.body.appendChild(table);
+
+  for (var d = 0; d < shopList.length;d++){
+    var shopTotal = 0;
+    var tableRow1 = document.createElement('tr');
+    var tableHead1 = document.createElement('th');
+    tableHead1.textContent = shopList[d].location;
+    tableRow1.appendChild(tableHead1);
+
+    for (var i = 0; i < hours.length; i++){
+      var tableData1 = document.createElement('td');
+      var hourlyTotal = shopList[d].randomNumberOfCust();
+      shopTotal += hourlyTotal;
+      tableData1.textContent = hourlyTotal.toFixed(2);
+      tableRow1.appendChild(tableData1);
+      console.log(i);
+    }
+
+      var tableData2 = document.createElement('td');
+      tableData2.textContent = shopTotal.toFixed(2);
+      tableRow1.appendChild(tableData2);
+      table.appendChild(tableRow1);
+    }
+    var tableHead = document.createElement('th');
+    tableHead.textContent = 'Total Daily Lbs.';
+    tableRow.appendChild(tableHead);
+      document.getElementById('customersperhour').appendChild(table);
+};
+var createCupsPerHourTable =  function(){
+  var section = document.getElementById('test');
+  var hours = ['6:00','7:00','8:00','9:00','10:00','11:00','12:00','1:00','2:00','3:00','4:00','5:00','6:00','7:00','8:00'];
+  var table = document.createElement('table');
+  var tableRow = document.createElement('tr');
+  var tableHead = document.createElement('th');
+  tableHead.textContent = 'Location';
+  tableRow.appendChild(tableHead);
+
+  for (var i = 0; i < hours.length; i++){
+    var tableHeadHours = document.createElement('th');
+    tableHeadHours.textContent = hours[i];
+    tableRow.appendChild(tableHeadHours);
+
+  }
+    table.appendChild(tableRow);
+//    document.body.appendChild(table);
+
+  for (var d = 0; d < shopList.length;d++){
+    var shopTotal = 0;
+    var tableRow1 = document.createElement('tr');
+    var tableHead1 = document.createElement('th');
+    tableHead1.textContent = shopList[d].location;
+    tableRow1.appendChild(tableHead1);
+
+    for (var i = 0; i < hours.length; i++){
+      var tableData1 = document.createElement('td');
+      var hourlyTotal = shopList[d].cupsPerCust;
+      console.log(hourlyTotal);
+      shopTotal += hourlyTotal;
+      tableData1.textContent = hourlyTotal.toFixed(2);
+      tableRow1.appendChild(tableData1);
+      console.log(i);
+    }
+
+      var tableData2 = document.createElement('td');
+      tableData2.textContent = shopTotal.toFixed(2);
+      tableRow1.appendChild(tableData2);
+      table.appendChild(tableRow1);
+    }
+    var tableHead = document.createElement('th');
+    tableHead.textContent = 'Total Daily Cups';
+    tableRow.appendChild(tableHead);
+      document.getElementById('cupsperhour').appendChild(table);
+};
 
 var pikePlaceMarket = new ShopLocation('Pike Place Market',14,55,1.2,3.7);
 var capitolHill = new ShopLocation('Capitol Hill',32,48,3.2,0.4);
@@ -84,6 +177,9 @@ var website = new ShopLocation('Website',3,6,0,6.7);
 
 var shopList =[pikePlaceMarket,capitolHill,seattlePublic,southLake,seaTac,website];
 createTable();
+createCustPerHourTable();
+createCupsPerHourTable();
+
 /*function tableHours(){
   var section = document.getElementById('test');
   var hours = ['6:00','7:00','8:00','9:00','10:00','11:00','12:00','1:00','2:00','3:00','4:00','5:00','6:00','7:00','8:00'];
